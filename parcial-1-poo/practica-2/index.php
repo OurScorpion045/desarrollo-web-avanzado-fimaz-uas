04999a78e7bfabdd13ca0d3617405dd05cb8176e
OurScorpion045 <sams24484@gmail.com>
2026-02-23 15:43:57 -0700

    Practica 2

---
+++ parcial-1-poo/practica-2/index.php
@@ -0,0 +1,14 @@
+<?php
+
+    require_once 'Admin.php';
+
+    $objAdmin01 = new Admin("Mario Hernandez", "sams24494@gmail.com");
+    
+    $nombreAdmin01 = $objAdmin01->getNombre();
+    $correoAdmin01 = $objAdmin01->getCorreo();
+    $rolAdmin01 = $objAdmin01->getRol();
+
+    echo "Nombre: $nombreAdmin01<br>";
+    echo "Correo: $correoAdmin01<br>";
+    echo "Rol: $rolAdmin01<br>"
+?>
