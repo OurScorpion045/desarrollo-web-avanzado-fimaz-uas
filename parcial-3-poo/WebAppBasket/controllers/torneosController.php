63ee2eb48e706ceed5f852cf93e0578499bb09e2
Mario Alejandro Hdez Lopez <sams24484@gmail.com>
2026-04-09 13:37:35 -0700

    Avance: Archivo torneosController.php, Evaluacion parcial

---
+++ parcial-3-poo/WebAppBasket/controllers/torneosController.php
@@ -0,0 +1,23 @@
+<?php
+
+    require_once "../../models/torneosModel.php";
+
+    class torneosController {
+        private $model;
+
+        public function __construct(){
+            $this->model = new torneosModel();
+        }
+
+        //Crearemos el metodo controlador que mandara llamar la funcion insert del modelo.
+        //Tambien mandara los parametros necesarios para guardar en la tabla "torneos".
+        //Si los datos se guardan redireccionara al usuario a la pantalla principal de inicio
+        //De lo contrario se mantendra en la pantalla del formulario de captura de datos del torneo
+
+        public function saveTorneo($nombreTorneo, $organizador, $patrocinadores, $sede, $categoria, $premio1, $premio2, $premio3, $usuario, $contrasena) {
+            $id = $this->model->insert($nombreTorneo, $organizador, $patrocinadores, $sede, $categoria, $premio1, $premio2, $premio3, $usuario, $contrasena);
+            return ($id != false) ? header("Location: mainTorneos.php") : header("Location: frmTorneos.php");
+        }
+    }
+
+?>
