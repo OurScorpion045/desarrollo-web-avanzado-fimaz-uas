d81119de060fb2dbffba367d9bbcda5f3d6ec6f1
Mario Alejandro Hdez Lopez <sams24484@gmail.com>
2026-04-09 13:22:18 -0700

    Avance: Archivo torneosModel.php, Evaluacion parcial

---
+++ parcial-3-poo/WebAppBasket/models/torneosModel.php
@@ -0,0 +1,55 @@
+<?php
+
+    require_once("../../config/DataBase.php");
+
+    class torneosModel {
+        public $PDO;
+
+        public function __construct() {
+            // Declaracion de la variable para conexion a la BD
+            // Se instancia la clase DataBase
+            $conecction = new Database();
+
+            //Llamamos al metodo connect y lo asignamos al atributo PDO de nuestra clase
+            $this->PDO = $conecction->connect();
+        }
+
+        public function insert($nombreTorneo, $organizador, $patrocinadores, $sede, $categoria, $premio1, $premio2, $premio3, $usuario, $contrasena) {
+            // Iniciamos declarando el statement y preparando la consulta
+            $statement = $this->PDO->prepare("INSERT INTO torneos VALUES(null, :nombreTorneo, :organizador, :patrocinadores, :sede, :categoria, :premio1, :premio2, :premio3, :usuario, :contrasena)");
+
+            //Encriptar contraseña asignada al organizador del torneo
+            $contrasena = $this->passwordEncrypyt($contrasena);
+            //Asociamos los valores colocados como placeholder en la sentencia SQL
+            $statement->bindParam(":nombreTorneo", $nombreTorneo);
+            $statement->bindParam(":organizador", $organizador);
+            $statement->bindParam(":patrocinadores", $patrocinadores);
+            $statement->bindParam(":sede", $sede);
+            $statement->bindParam(":categoria", $categoria);
+            $statement->bindParam("premio1", $premio1);
+            $statement->bindParam("premio2", $premio2);
+            $statement->bindParam("premio3", $premio3);
+            $statement->bindParam(":usuario", $usuario);
+            $statement->bindParam(":contrasena", $contrasena);
+            
+            //Ejecutamos el statement mediante execute(). Valoraremos mediante un shorthand if lo que regresara este método
+            return ($statement->execute()) ? $this->PDO->lastInsertId() : false;
+        }
+
+        //El administrador creara el torneo y al usuario organizador
+        //Por lo que al crear su password, buscaremos encriptarla por seguridad
+        //Utilizaremos el metodo password_hash y password_verify
+
+        public function passwordEncrypyt($password) {
+            $passwordEncrypted = password_hash($password, PASSWORD_DEFAULT);
+            return $passwordEncrypted;
+        }
+
+        //Metodo para verificar si la contraseña introducida corresponde con la encriptada
+        public function passwordDencrypted($passwordEncrypted, $passwordCandidate) {
+            // Se verifica con un shorthand if si el password candidato es correcto
+            return (password_verify($passwordCandidate, $passwordEncrypted)) ? true : false;
+        }
+    }
+
+?>
