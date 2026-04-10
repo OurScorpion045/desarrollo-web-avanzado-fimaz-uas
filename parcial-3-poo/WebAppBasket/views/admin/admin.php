3cbe623c8bae2cef68e52558c8e4b8e33ba96086
Mario Alejandro Hdez Lopez <sams24484@gmail.com>
2026-04-10 02:15:15 -0700

    Avance: archivos footer.php, admin.php, Evaluacion parcial

--- parcial-3-poo/WebAppBasket/views/admin/admin.php
+++ parcial-3-poo/WebAppBasket/views/admin/admin.php
@@ -1,75 +1,76 @@
 <?php
     require_once("../admin/template/header.php");
 ?>
-
-<div class="card text-center">
-    <div class="card-header">
-        MENU
-    </div>
-    <div class="card-body">
-        <h5 class="card-title"></h5>
-            <div class="row mb-3">
-                <div class="col">
-                    <div class="card text-center">
-                    <div class="card-header">
-                        CREAR TORNEO
-                    </div>
-                    <div class="card-body">
-                    
-                    <a href="frmTorneos.php" class="btn btn-primary">
-                        <img src="../img/Torneo.jpg" alt="Crear un torneo." width="200" height="180">
-                    </a>
-
-                    </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card text-center">
-                    <div class="card-header">
-                        LISTA DE TORNEOS
-                    </div>
-                    <div class="card-body">
-                    
-                        <a href="readAllTorneos.php" class="btn btn-primary">
-                            <img src="../img/lista-torneo.jpg" alt="Listar torneo." width="200" height="180">
+<div class="mx-auto p-5">
+    <div class="card text-center">
+        <div class="card-header">
+            MENU
+        </div>
+        <div class="card-body">
+            <h5 class="card-title"></h5>
+                <div class="row mb-3">
+                    <div class="col">
+                        <div class="card text-center">
+                        <div class="card-header">
+                            CREAR TORNEO
+                        </div>
+                        <div class="card-body">
+                        
+                        <a href="frmTorneos.php" class="btn btn-primary">
+                            <img src="../img/Torneo.jpg" alt="Crear un torneo." width="200" height="180">
                         </a>
 
+                        </div>
+                        </div>
                     </div>
+                    <div class="col">
+                        <div class="card text-center">
+                        <div class="card-header">
+                            LISTA DE TORNEOS
+                        </div>
+                        <div class="card-body">
+                        
+                            <a href="readAllTorneos.php" class="btn btn-primary">
+                                <img src="../img/lista-torneo.jpg" alt="Listar torneo." width="200" height="180">
+                            </a>
+
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <!--AÑADIREMOS OTRA FILA CON DOS CARDS-->
+                <div class="row">
+                    <div class="col">
+                        <div class="card text-center">
+                        <div class="card-header">
+                            ESTADISTICAS
+                        </div>
+                        <div class="card-body">
+                        
+
+
+
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card text-center">
+                        <div class="card-header">
+                            ANUNCIOS
+                        </div>
+                        <div class="card-body">
+                        
+
+
+
+                        </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <!--AÑADIREMOS OTRA FILA CON DOS CARDS-->
-            <div class="row">
-                <div class="col">
-                    <div class="card text-center">
-                    <div class="card-header">
-                        ESTADISTICAS
-                    </div>
-                    <div class="card-body">
-                    
-
-
-
-                    </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card text-center">
-                    <div class="card-header">
-                        ANUNCIOS
-                    </div>
-                    <div class="card-body">
-                    
-
-
-
-                    </div>
-                    </div>
-                </div>
-            </div>
+        <div class="card-footer text-body-secondary">
+            Configuracion de torneos. Web App BasketBall.
         </div>
-    <div class="card-footer text-body-secondary">
-        Configuracion de torneos. Web App BasketBall.
     </div>
 </div>
 
--- parcial-3-poo/WebAppBasket/views/admin/template/footer.php
+++ parcial-3-poo/WebAppBasket/views/admin/template/footer.php
@@ -1,8 +1,8 @@
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
   </body>
-  <footer class="text-center text-lg-start bg-white text-muted">
-    <section>
+  <footer class="text-center text-lg-start bg-light text-muted">
+    <section class="p-2">
         <div class="container text-center text-md-start mt-5">
             <div class="row mt-3">
                 <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
@@ -23,12 +23,18 @@
                 <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-4">
                     <h6 class="text-uppercase fw-bold mb4">CONTACTO</h6>
                     <p>
-                        CORREO, ETC.
-                        Facultad de Informatica Mazatlán, Universidad Autonoma de Sinaloa.
+                        <ol>
+                            <li>+52 6699811560</li>
+                            <li>sams24484@gmail.com</li>
+                            <li>Facultad de Informatica Mazatlán, Universidad Autonoma de Sinaloa.</li>
+                        </ol>
                     </p>
                 </div>
             </div>
         </div>
     </section>
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.25)">
+        © Clase Desarrollo web Avanzado. MVC, PHP, PDO, MySQL, BOOTSTRAP.
+    </div>
   </footer>
 </html>
